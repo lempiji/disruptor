ad84fd54d25d036d996dfd08f6889ac4b19f854e
lempiji <lempiji@users.noreply.github.com>
2025-07-09 07:19:39 +0900

    Add FixedSequenceGroup

--- source/disruptor/package.d
+++ source/disruptor/package.d
@@ -3,5 +3,6 @@ module disruptor;
 // Disruptor port in D - work in progress
 public import disruptor.sequence;
 public import disruptor.sequencegroup;
+public import disruptor.fixedsequencegroup;
 public import disruptor.sequencer;
 public import disruptor.waitstrategy;
