1e0f9df4d669fd0782964a41f66a75c161d71cb6
lempiji <lempiji@users.noreply.github.com>
2025-07-08 21:36:03 +0900

    Add SequenceGroup implementation

---
+++ source/disruptor/sequencegroup.d
@@ -0,0 +1,149 @@
+module disruptor.sequencegroup;
+
+import core.atomic;
+import core.internal.atomic : atomicLoad;
+import disruptor.sequence;
+import disruptor.sequencer : Cursored;
+
+long getMinimumSequence(shared const(Sequence)[] sequences, long minimum = long.max) @nogc nothrow
+{
+    auto min = minimum;
+    foreach (s; sequences)
+    {
+        auto value = s.get();
+        if (value < min)
+            min = value;
+    }
+    return min;
+}
+
+private int countMatching(shared const(Sequence)[] values, shared const(Sequence) toMatch) @nogc nothrow
+{
+    int count = 0;
+    foreach (v; values)
+    {
+        if (v is toMatch)
+            ++count;
+    }
+    return count;
+}
+
+void addSequences(shared Sequence[]* field, Cursored cursor, shared Sequence[] sequencesToAdd...)
+{
+    long cursorSequence;
+    shared Sequence[] updated;
+    shared Sequence[] current;
+    do
+    {
+        current = atomicLoad!(MemoryOrder.acq)(field);
+        updated = new shared Sequence[](current.length + sequencesToAdd.length);
+        updated[0 .. current.length] = current[];
+        cursorSequence = cursor.getCursor();
+        size_t index = current.length;
+        foreach (seq; sequencesToAdd)
+        {
+            seq.set(cursorSequence);
+            updated[index++] = seq;
+        }
+    }
+    while (!cas(field, current, updated));
+
+    cursorSequence = cursor.getCursor();
+    foreach (seq; sequencesToAdd)
+    {
+        seq.set(cursorSequence);
+    }
+}
+
+bool removeSequence(shared Sequence[]* field, shared Sequence sequence)
+{
+    int toRemove;
+    shared Sequence[] oldSeqs;
+    shared Sequence[] newSeqs;
+    do
+    {
+        oldSeqs = atomicLoad!(MemoryOrder.acq)(field);
+        toRemove = countMatching(oldSeqs, sequence);
+        if (toRemove == 0)
+            break;
+        newSeqs = new shared Sequence[](oldSeqs.length - toRemove);
+        size_t pos = 0;
+        foreach (s; oldSeqs)
+        {
+            if (s !is sequence)
+                newSeqs[pos++] = s;
+        }
+    }
+    while (!cas(field, oldSeqs, newSeqs));
+    return toRemove != 0;
+}
+
+class SequenceGroup : Sequence
+{
+private:
+    align(16) shared Sequence[] sequences = [];
+
+public:
+    this() shared
+    {
+        super(Sequence.INITIAL_VALUE);
+    }
+
+    override long get() const shared @nogc nothrow
+    {
+        auto seqs = atomicLoad!(MemoryOrder.acq)(&sequences);
+        return getMinimumSequence(seqs);
+    }
+
+    override void set(long value) shared @nogc nothrow
+    {
+        auto seqs = atomicLoad!(MemoryOrder.acq)(&sequences);
+        foreach (s; seqs)
+            s.set(value);
+    }
+
+    void add(shared Sequence sequence) shared
+    {
+        shared Sequence[] oldSeqs;
+        shared Sequence[] newSeqs;
+        do
+        {
+            oldSeqs = atomicLoad!(MemoryOrder.acq)(&sequences);
+            auto oldSize = oldSeqs.length;
+            newSeqs = new shared Sequence[](oldSize + 1);
+            newSeqs[0 .. oldSize] = oldSeqs[];
+            newSeqs[oldSize] = sequence;
+        }
+        while (!cas(&sequences, oldSeqs, newSeqs));
+    }
+
+    bool remove(shared Sequence sequence) shared
+    {
+        return removeSequence(&sequences, sequence);
+    }
+
+    int size() const shared @nogc nothrow
+    {
+        return cast(int) atomicLoad!(MemoryOrder.acq)(&sequences).length;
+    }
+
+    void addWhileRunning(Cursored cursor, shared Sequence sequence) shared
+    {
+        addSequences(&sequences, cursor, sequence);
+    }
+}
+
+unittest
+{
+    auto group = new shared SequenceGroup();
+    assert(group.get == long.max);
+    auto seq1 = new shared Sequence();
+    auto seq2 = new shared Sequence();
+    group.add(seq1);
+    group.add(seq2);
+    assert(group.size == 2);
+    group.set(5);
+    assert(seq1.get == 5 && seq2.get == 5);
+    assert(group.remove(seq1));
+    assert(group.size == 1);
+}
