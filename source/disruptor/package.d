765e78c7b81dc118d85d1827252ee4c15b94cb5d
lempiji <lempiji@users.noreply.github.com>
2025-07-10 08:33:09 +0900

    Refine ProcessingSequenceBarrier

--- source/disruptor/package.d
+++ source/disruptor/package.d
@@ -5,5 +5,6 @@ public import disruptor.sequence;
 public import disruptor.sequencegroup;
 public import disruptor.fixedsequencegroup;
 public import disruptor.sequencer;
+public import disruptor.processingsequencebarrier;
 public import disruptor.waitstrategy;
 public import disruptor.util;
